6a27f8087b2afeec573ae72d24364cf639e61de7
A L 3 X   V 3 G A S <officialalexvegas@gmail.com>
2016-05-07 18:50:57 +0200

    Added files via upload

---
+++ code/install.sh
@@ -0,0 +1,39 @@
+# This is the official installer script for BAV3
+# It will install all filea and create all needed directories.
+echo "[AVIA] Hi, My name is Avia. I will oversee the setup process of your Bave installation."
+echo "[AVIA] First we have to install some dependencies. These will not take up a lot of space. "
+echo "[AVIA] Installation starting in..."
+echo "[AVIA] 5"
+sleep 1
+echo "[AVIA] 4"
+sleep 1
+echo "[AVIA] 3"
+sleep 1
+echo "[AVIA] 2"
+sleep 1
+echo "[AVIA] 1"
+sleep 1
+echo "[AVIA] 0"
+sleep 1
+echo ""
+echo ""
+echo "[AVIA] Updating package lists..."
+apt-get update
+echo "[AVIA] ...done."
+echo ""
+echo "[AVIA] Installing Python 2..."
+apt-get install python2
+echo "[AVIA] ...done."
+echo ""
+echo "[AVIA] Installing Git..."
+apt-get install python2
+echo "[AVIA] ...done."
+echo ""
+echo "[AVIA] Finished installing dependecies."
+echo ""
+echo "[AVIA] Starting second part of installation..."
+echo "[AVIA] Starting auxiliary installer script..."
+echo "[AVIA] ...done."
+echo "[AVIA] Generating uninstaller script..."
+echo "[AVIA] ...done."
+echo "[AVIA] Installation of Bave hast finished!"
